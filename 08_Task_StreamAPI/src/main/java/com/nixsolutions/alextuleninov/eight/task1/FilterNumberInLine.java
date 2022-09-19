5d304a45a58c29630fe89bdc2623adfc692567c5
Alexander Tuleninov <talextu@gmail.com>
2022-09-19 13:40:01 +0300

    seven commit - 08_Task_IO

--- 08_Task_StreamAPI/src/main/java/com/nixsolutions/alextuleninov/eight/task1/FilterNumberInLine.java
+++ 08_Task_StreamAPI/src/main/java/com/nixsolutions/alextuleninov/eight/task1/FilterNumberInLine.java
@@ -5,8 +5,12 @@
 
 package com.nixsolutions.alextuleninov.eight.task1;
 
+import java.util.Arrays;
+import java.util.OptionalLong;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
+import static java.lang.System.err;
 import static java.lang.System.out;
 
 /**
@@ -20,12 +24,37 @@ public final class FilterNumberInLine {
     public static void main(String[] args) {
 
         FilterNumberInLine f = new FilterNumberInLine();
-        f.filterNumberInLine(new String[]{"string 1 text", "2 string 3 text", "45"});
+        f.filterNumberInLineToConsole(new String[]{"string 1 text", "2 string 3 text", "45"});
         out.println();
 
-        f.filterNumberInLine(null);
+        f.filterNumberInLineToConsole(null);
+        f.filterNumberInLineToConsole(new String[]{});
         out.println();
 
+        f.filterNumberInLineToConsole1(new String[]{"string 1 text", "2 string 3 text", "45"});
+        out.println();
+
+        out.print(f.filterNumberInLineToString(new String[]{"string 1 text", "2 string 3 text", "45"}));
+        out.print(f.filterNumberInLineToString(null));
+        out.println();
+
+        out.print(f.filterNumberInLineToOptionalLong(new String[]{"string 1 text", "2 string 3 text", "45"}));
+        out.println();
+        out.print(f.filterNumberInLineToOptionalLong(null));
+        out.println();
+
+        out.print(f.filterNumberInLineToLong(new String[]{"string 1 text", "2 string 3 text", "45"}));
+        out.println();
+        try {
+            out.print(f.filterNumberInLineToLong(new String[]{}));
+        } catch (IllegalArgumentException e) {
+            err.println("Input contains no valid digits");
+        }
+        try {
+            out.print(f.filterNumberInLineToLong(null));
+        } catch (NullPointerException e) {
+            err.println("Input null");
+        }
     }
 
     /**
@@ -34,9 +63,9 @@ public final class FilterNumberInLine {
      *
      * @param string the array of rows
      */
-    public void filterNumberInLine(String[] string) {
-        if (string == null) {
-            out.print("Argument must be not null");
+    public void filterNumberInLineToConsole(String[] string) {
+        if (string == null || string.length == 0) {
+            err.println("Argument must be not null and length must be not 0");
             return;
         }
 
@@ -48,9 +77,9 @@ public final class FilterNumberInLine {
         m.forEach(out::print);
     }
 
-    public void filterNumberInLine1(String[] string) {
-        if (string == null) {
-            out.print("Argument must be not null");
+    public void filterNumberInLineToConsole1(String[] string) {
+        if (string == null || string.length == 0) {
+            err.println("Argument must be not null and length must be not 0");
             return;
         }
 
@@ -59,6 +88,40 @@ public final class FilterNumberInLine {
         joinedString.codePoints()
                 .filter(Character::isDigit)
                 .map(s -> Character.digit(s, 10)).forEach(out::print);
+    }
+
+    public String filterNumberInLineToString(String[] string) {
+        if (string == null || string.length == 0) {
+            err.println("Argument must be not null and length must be not 0");
+            return "";
+        }
+
+        return Arrays.stream(string)
+                .flatMap(s -> Stream.of(s.split("")))
+                .filter(s -> Character.isDigit(s.charAt(0)))
+                .reduce("", String::concat);
+    }
+
+    public OptionalLong filterNumberInLineToOptionalLong(String[] string) {
+        if (string == null || string.length == 0) {
+            return OptionalLong.empty();
+        }
+        return Arrays.stream(string)
+                .flatMapToInt(String::codePoints)
+                .filter(Character::isDigit)
+                .map(s -> Character.digit(s, 10))
+                .asLongStream()
+                .reduce((number, s) -> number * 10 + s);
+    }
+
+    public long filterNumberInLineToLong(String[] string) {
+        return Arrays.stream(string)
+                .flatMapToInt(String::codePoints)
+                .filter(Character::isDigit)
+                .map(s -> Character.digit(s, 10))
+                .asLongStream()
+                .reduce((number, s) -> number * 10 + s)
+                .orElseThrow(() -> new IllegalArgumentException("Input contains no valid digits: " + string));
     }
 
 }
